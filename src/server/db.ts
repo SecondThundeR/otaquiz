b9e978b5364a62e824bdf83ea31f7237a3c7d905
SecondThundeR <awayfromgalaxy@gmail.com>
2023-06-19 01:00:43 +0300

    feat(db): add `pretty` error format for Prisma errors

--- src/server/db.ts
+++ src/server/db.ts
@@ -10,6 +10,7 @@ export const prisma =
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    errorFormat: `pretty`,
   });
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
