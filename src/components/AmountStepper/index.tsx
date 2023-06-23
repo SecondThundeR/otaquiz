61585a6b13846d4c91dff3bd5ca03c376f3fea0e
SecondThundeR <awayfromgalaxy@gmail.com>
2023-06-23 15:23:06 +0300

    feat(containers): add AmountStepper
    This component used for selection amount of animes to guess

---
+++ src/components/AmountStepper/index.tsx
@@ -0,0 +1,25 @@
+import { MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
+
+interface AmountStepperProps {
+  amount: number;
+  increment: () => void;
+  decrement: () => void;
+}
+
+export default function AmountStepper({
+  amount,
+  increment,
+  decrement,
+}: AmountStepperProps) {
+  return (
+    <div className="flex items-center justify-center gap-4">
+      <button className="btn-outline btn-square btn" onClick={increment}>
+        <PlusIcon className="h-6 w-6" />
+      </button>
+      <h1 className="font-accent text-4xl">{amount}</h1>
+      <button className="btn-outline btn-square btn" onClick={decrement}>
+        <MinusIcon className="h-6 w-6" />
+      </button>
+    </div>
+  );
+}
