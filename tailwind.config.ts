7ccf50ea6256915e1bf0395ca5e9b1fa2e5b2ac7
SecondThundeR <awayfromgalaxy@gmail.com>
2023-06-23 14:32:43 +0300

    feat: configure daisyui themes
    For now, default "light" and "synthwave" are used

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,5 +5,9 @@ export default {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: ["light", "synthwave"],
+    darkTheme: "synthwave",
+  },
   plugins: [require("daisyui")],
 } satisfies Config;
