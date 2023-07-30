a7cf0cf683bfed041821bb73844c458bc0872e5b
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-30 18:33:59 +0300

    fix(components): remove `blur` placeholder support

--- src/components/Screenshot/index.tsx
+++ src/components/Screenshot/index.tsx
@@ -1,13 +1,11 @@
 import Image from "next/image";
 
-function Screenshot({ src, blurSrc }: { src: string; blurSrc: string }) {
+function Screenshot({ src }: { src: string }) {
   return (
     <Image
       width={1280}
       height={720}
       src={src}
-      placeholder="blur"
-      blurDataURL={blurSrc}
       priority
       className="h-auto w-96 rounded-xl"
       alt=""
