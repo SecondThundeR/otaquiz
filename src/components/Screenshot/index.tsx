d4673d390d7da349b46777e18d83c2028d0b6a2a
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-30 23:32:20 +0300

    feat(components): update `Screenshot`
    Add `ScreenshotPlaceholder`
    Add support for `useOnImageLoad`
    Add fixed size of screenshot image
    Remove unnecessary props of `Image` component

--- src/components/Screenshot/index.tsx
+++ src/components/Screenshot/index.tsx
@@ -1,15 +1,28 @@
+import cn from "classnames";
 import Image from "next/image";
 
+import { SCREENSHOT_CLASSES } from "@/constants/screenshotClasses";
+import { useOnImageLoad } from "@/hooks/useOnImageLoad";
+
+import { ScreenshotPlaceholder } from "../ScreenshotPlaceholder";
+
 function Screenshot({ src }: { src: string }) {
+  const [isLoading, onLoad] = useOnImageLoad();
+
   return (
-    <Image
-      width={1280}
-      height={720}
-      src={src}
-      priority
-      className="h-auto w-96 rounded-xl"
-      alt=""
-    />
+    <>
+      <ScreenshotPlaceholder isLoading={isLoading} />
+      <Image
+        width={1280}
+        height={720}
+        src={src}
+        className={cn(SCREENSHOT_CLASSES, {
+          hidden: isLoading,
+        })}
+        alt=""
+        onLoadingComplete={onLoad}
+      />
+    </>
   );
 }
 
---
+++ src/constants/screenshotClasses.ts
@@ -0,0 +1,2 @@
+export const SCREENSHOT_CLASSES =
+  "h-64 w-96 rounded-xl object-cover sm:w-[512px]";
