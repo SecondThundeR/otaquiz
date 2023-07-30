10d4a864e160ea0de88ec1f16d7393a9ef327fa4
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-30 18:29:57 +0300

    refactor(routers): remove example router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,16 +1,8 @@
-import { exampleRouter } from "@/server/api/routers/example";
 import { gameRouter } from "@/server/api/routers/game";
 import { createTRPCRouter } from "@/server/api/trpc";
 
-/**
- * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here.
- */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   game: gameRouter,
 });
 
-// export type definition of API
 export type AppRouter = typeof appRouter;
