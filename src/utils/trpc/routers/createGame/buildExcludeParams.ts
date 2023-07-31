f026450a7042524fda568001017f64f658fa310a
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-31 20:27:32 +0300

    fix(utils): include `originalUrl` in `buildExcludeParams`

--- src/utils/trpc/routers/createGame/buildExcludeParams.ts
+++ src/utils/trpc/routers/createGame/buildExcludeParams.ts
@@ -11,6 +11,7 @@ export const buildExcludeParams = (excludeIds: string | null) => {
           russian
           screenshots {
             id
+            originalUrl
           }
         }
       }`,
