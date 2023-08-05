8d46ec0be9ca464333a451fc18b17b16b1d56e6a
SecondThundeR <awayfromgalaxy@gmail.com>
2023-08-05 16:12:31 +0300

    feat(routers): add router for game history

---
+++ src/server/api/routers/history.ts
@@ -0,0 +1,20 @@
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+
+import { processError } from "@/utils/trpc/processError";
+
+export const historyRouter = createTRPCRouter({
+  getGameHistory: protectedProcedure.query(
+    async ({ ctx: { prisma, session } }) => {
+      try {
+        return await prisma.game.findMany({
+          where: {
+            userId: session.user.id,
+            isFinished: true,
+          },
+        });
+      } catch (error: unknown) {
+        processError(error);
+      }
+    },
+  ),
+});
