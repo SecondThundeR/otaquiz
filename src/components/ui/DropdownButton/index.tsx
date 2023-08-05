960eae65e6bbe1bcd963ec4aab879f673eef39d8
SecondThundeR <awayfromgalaxy@gmail.com>
2023-08-05 16:12:52 +0300

    feat(components): add support for `next/link` in `DropdownButton`

--- src/components/ui/DropdownButton/index.tsx
+++ src/components/ui/DropdownButton/index.tsx
@@ -1,18 +1,28 @@
+import { default as NextLink } from "next/link";
 import { type PropsWithChildren, memo } from "react";
 
 import { Link } from "../Link";
 
 type DropdownButtonProps = PropsWithChildren<{
   href?: string;
+  to?: string;
   onClick?: () => void;
 }>;
 
 export const DropdownButton = memo(function DropdownButton({
   children,
   href,
+  to,
   onClick,
 }: DropdownButtonProps) {
   const isButton = !href;
+
+  if (to)
+    return (
+      <li>
+        <NextLink href={to}>{children}</NextLink>
+      </li>
+    );
 
   return (
     <li>
