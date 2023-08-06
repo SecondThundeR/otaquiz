25ff6c044b0797669367d7d4769ac64fe764b645
SecondThundeR <awayfromgalaxy@gmail.com>
2023-08-06 21:51:59 +0300

    feat(components): add `IconLink` component

---
+++ src/components/Navbar/IconLink/index.tsx
@@ -0,0 +1,20 @@
+import { type Icon } from "@primer/octicons-react";
+import { memo } from "react";
+
+import { Link } from "@/components/ui/Link";
+
+interface IconLinkProps {
+  icon: Icon;
+  href: string;
+}
+
+export const IconLink = memo(function IconLink({
+  icon: Icon,
+  href,
+}: IconLinkProps) {
+  return (
+    <Link isStyled={false} isHover={false} href={href}>
+      <Icon className="h-7 w-7 hover:fill-primary" />
+    </Link>
+  );
+});
