c6a0653c036ed23c8f8e6e329c6896a7ce81857e
SecondThundeR <awayfromgalaxy@gmail.com>
2023-08-07 19:11:35 +0300

    chore: update ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,7 +18,15 @@ const config = {
         fixStyle: "inline-type-imports",
       },
     ],
-    "@typescript-eslint/no-misused-promises": "off",
+    "@typescript-eslint/no-misused-promises": [
+      2,
+      {
+        checksVoidReturn: {
+          attributes: false,
+          returns: false,
+        },
+      },
+    ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
   },
 };
