9feca4432efd574f745f635fdc8ef4260ad04321
SecondThundeR <awayfromgalaxy@gmail.com>
2023-08-09 15:59:47 +0300

    fix(ui): remove `h-full` from `ButtonsGrid` div

--- src/ui/ButtonsGrid/index.tsx
+++ src/ui/ButtonsGrid/index.tsx
@@ -4,7 +4,7 @@ export const ButtonsGrid = memo(function ButtonsGrid({
   children,
 }: PropsWithChildren) {
   return (
-    <div className="grid h-full w-full grid-cols-1 grid-rows-1 gap-4 sm:grid-cols-2">
+    <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2">
       {children}
     </div>
   );
