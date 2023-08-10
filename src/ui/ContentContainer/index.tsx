09b773555436560195dc13af4f2fe9c51c8414a5
SecondThundeR <awayfromgalaxy@gmail.com>
2023-08-10 11:19:45 +0300

    fix(ui): tweak `ContentContainer` margin and gap

--- src/ui/ContentContainer/index.tsx
+++ src/ui/ContentContainer/index.tsx
@@ -2,7 +2,7 @@ import { type PropsWithChildren } from "react";
 
 export function ContentContainer({ children }: PropsWithChildren) {
   return (
-    <div className="container mx-auto mb-6 mt-7 flex flex-grow flex-col items-center justify-center gap-5 px-6">
+    <div className="container mx-auto my-4 flex flex-grow flex-col items-center justify-center gap-4 px-6">
       {children}
     </div>
   );
