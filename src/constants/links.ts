d20114307c2ccb3d447fa58ac57e03d6e2ec55aa
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2024-03-23 00:11:45 +0300

    fix: correct ESLint errors

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,6 +1,6 @@
 export const SHIKIMORI_URL = "https://shikimori.one/";
-export const SHIKIMORI_ANIMES_URL = `${SHIKIMORI_URL}animes/` as const;
-export const SHIKIMORI_API_DOCS = `${SHIKIMORI_URL}api/doc/` as const;
-export const SHIKIMORI_GRAPHQL_API_URL = `${SHIKIMORI_URL}api/graphql` as const;
+export const SHIKIMORI_ANIMES_URL = `${SHIKIMORI_URL}animes/`;
+export const SHIKIMORI_API_DOCS = `${SHIKIMORI_URL}api/doc/`;
+export const SHIKIMORI_GRAPHQL_API_URL = `${SHIKIMORI_URL}api/graphql`;
 export const GITHUB_ACCOUNT_LINK = "https://github.com/SecondThundeR/";
-export const GITHUB_REPO_LINK = `${GITHUB_ACCOUNT_LINK}otaquiz` as const;
+export const GITHUB_REPO_LINK = `${GITHUB_ACCOUNT_LINK}otaquiz`;
